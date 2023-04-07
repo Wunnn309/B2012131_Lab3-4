a50209fc0094893deef4491efba15cff54d0bb6b
Wunnn309 <minhquan300902@gmail.com>
2023-04-07 14:32:53 +0700

    Tao trang cap nhat lien he

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,13 +1,15 @@
 import axios from "axios";
+
 const commonConfig = {
-headers: {
-"Content-Type": "application/json",
-Accept: "application/json",
-},
+  header: {
+    "Content-Type": "application/jon",
+    Accept: "application/json",
+  },
 };
+
 export default (baseURL) => {
-return axios.create({
-baseURL,
-...commonConfig,
-});
+  return axios.create({
+    baseURL,
+    ...commonConfig,
+  });
 };
